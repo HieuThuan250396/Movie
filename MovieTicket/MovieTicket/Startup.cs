13067cd421f6bcca50a099d531ae7a3e5762f4d5
JEREMY\JeremyPC <trungthejd@gmail.com>
2017-10-25 13:23:24 +0700

    trang login, register

---
+++ MovieTicket/MovieTicket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieTicket.Startup))]
+namespace MovieTicket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
