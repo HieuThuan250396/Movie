8a4d28011bc4e4488707d291bc231832f5fdac63
JEREMY\JeremyPC <trungthejd@gmail.com>
2017-10-26 19:25:10 +0700

    MovieInfo

---
+++ MovieTicket/MovieTicket/Controllers/MovieInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieTicket.Controllers
+{
+    public class MovieInfoController : Controller
+    {
+        // GET: MovieInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
