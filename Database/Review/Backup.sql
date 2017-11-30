e9659731df070943dfee3e60ce5f82b7fc8a357a
MISU\THUAN <hieuthuan250396@gmail.com>
2017-11-30 16:26:35 +0700

    update slide

--- Database/Review/Backup.sql
+++ Database/Review/Backup.sql
@@ -16,16 +16,5 @@ GO
 BACKUP LOG qldv  
    TO  DISK ='E:\qldv_log.Bak'  
 GO 
---chua biet huong lam
-use qldv
-go
-declare @masuatchieu int = (select VeDangDat.masuatchieu from VeDangDat)
-declare @giodat datetime = (select VeDangDat.giodat from VeDangDat where VeDangDat.mave = 1) 
-declare @gioketthuc datetime = (select (cast(SuatChieu.ngaychieu as datetime) + cast(SuatChieu.giochieu as datetime)) from SuatChieu where  SuatChieu.masuatchieu  = 1)
-if((select datediff(minute,@giodat,@gioketthuc)) = 20)
-	print 'a'
-else 
-	print 'b'
 
-select * from VeDangDat
 --
--- Database/Review/PhanQuyen.sql
+++ Database/Review/PhanQuyen.sql
@@ -1,11 +1,21 @@
+--use qldv
+--go
+--create login administrator with password = 'admin@123'
+--go
+--create user admin1 for login administrator 
+--go
+--create role Administrator
+--go
+--grant  to Administrator
+--go
+--Sp_AddRoleMember 'Administrator', 'admin1'
+
+
 use qldv
 go
-create login administrator with password = 'admin@123'
+create login nhanvien with password = 'nhanvien'
 go
-create user admin1 for login administrator 
+create role NhanVien
+grant select, update, delete on KhachHang to NhanVien
 go
-create role Administrator
-go
-grant all to Administrator
-go
-Sp_AddRoleMember 'Administrator', 'admin1'
+Sp_AddRoleMember 'nhanvien', 'NhanVien'
