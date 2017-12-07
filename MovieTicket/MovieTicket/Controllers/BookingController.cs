603cedf4ab7a1db55467ba3841f0bd3eff09fbb5
MISU\THUAN <hieuthuan250396@gmail.com>
2017-12-07 15:45:10 +0700

    them procedure load ve theo suat chieu

--- MovieTicket/MovieTicket/Controllers/BookingController.cs
+++ MovieTicket/MovieTicket/Controllers/BookingController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieTicket.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,16 @@ namespace MovieTicket.Controllers
 {
     public class BookingController : Controller
     {
+        qldvEntities2 db = new qldvEntities2();
         // GET: Booking
-        public ActionResult Index()
+        public ActionResult Index(string suatChieu)
         {
-            return View();
+            if (!String.IsNullOrEmpty(suatChieu))
+            {
+                return View(db.Database.SqlQuery<SuatChieu>("exec sp_loadSuatChieu").ToList());
+            }
+            else
+                return View();
         }
     }
 }
