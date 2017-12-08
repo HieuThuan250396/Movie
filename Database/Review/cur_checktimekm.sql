ae210a025874843f3d407e09c52222a2d74da9f8
HoangMinh <vuhoangminh199@gmail.com>
2017-12-09 06:42:08 +0700

    update khuyen mai

--- Database/Review/cur_checktimekm.sql
+++ Database/Review/cur_checktimekm.sql
@@ -5,12 +5,11 @@ declare cur cursor
 dynamic
 for 
 	select makm from KhuyenMai where KhuyenMai.ngayketthuc <= GETDATE()
-
 declare @makm int
 open cur
 fetch next from cur into @makm
 while @@FETCH_STATUS=0
-	update KhuyenMai set giatri = 0 where makm = @makm
+	update KhuyenMai set tinhtrang = 0 where makm = @makm
 	fetch next from cur into @makm
 close cur 
 deallocate cur 
