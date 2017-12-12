525a34652c3acf4a044ae7cdc4e9ca517f2ab029
MISU\THUAN <hieuthuan250396@gmail.com>
2017-12-12 15:52:09 +0700

    job

---
+++ Database/Review/Job.sql
@@ -0,0 +1,78 @@
+﻿USE [msdb]
+GO
+
+/****** Object:  Job [CheckVeChuaThanhToan]    Script Date: 12/12/2017 3:50:33 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 12/12/2017 3:50:33 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'CheckVeChuaThanhToan', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'Check các vé gần giờ chiếu và chưa thanh toán', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'MISU\THUAN', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [CheckVeChuaThanhToan]    Script Date: 12/12/2017 3:50:33 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'CheckVeChuaThanhToan', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'declare @mave int , @masuatchieu int
+
+select 
+		@mave = mave, @masuatchieu = VeDangDat.masuatchieu
+from 
+		VeDangDat, SuatChieu
+where	VeDangDat.masuatchieu = SuatChieu.masuatchieu and 
+		datediff(minute,''01-01-2018 19:50:00'', cast(ngaychieu as datetime) + cast(giochieu as datetime)) <= 20 and tinhtrang = 0
+
+delete from vedangdat where mave = @mave and masuatchieu = @masuatchieu
+update ve set makhachhang = null, tinhtrang = 0, giodat = null, makm = null where mave = @mave and masuatchieu = @masuatchieu', 
+		@database_name=N'qldv', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'CheckVeSchedule', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=2, 
+		@freq_subday_interval=10, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20171201, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959, 
+		@schedule_uid=N'466a2952-a640-487b-ae9c-1363efd6b145'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+
