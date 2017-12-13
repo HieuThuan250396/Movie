200da694bb3982bc0c2ab66f6bdcc0cb873c32c1
DESKTOP-72O5CQT\NhanNhan <toi.ten.nhan@gmail.com>
2017-12-13 15:06:11 +0700

    Fix + Check OK file PhanQuyen

--- Database/Review/PhanQuyen.sql
+++ Database/Review/PhanQuyen.sql
@@ -13,29 +13,35 @@
 
 use qldv
 go
+
+-- nhan vien ban ve
 create login L_nhanvienbanve with password = 'nhanvienbanve'
 go
 create user U_nhanvienbanve for login L_nhanvienbanve
 go
-create role R_Nhanvienbanve
-grant select, update on NhanVien to R_Nhanvienbanve
-grant select,update on KhuyenMai to R_Nhanvienbanve
-grant select,update on PhongChieu to R_Nhanvienbanve
-grant select,update on KhachHang to R_Nhanvienbanve 
+create role R_NhanVienBanVe
+grant select on VeDangDat to R_NhanVienBanVe
+grant select, update on Ve to R_NhanVienBanVe
+grant select, insert, update on KhachHang to R_NhanVienBanVe
 go
-Sp_AddRoleMember 'R_Nhanvienbanve', 'U_nhanvienbanve'
+Sp_AddRoleMember 'R_NhanVienBanVe', 'U_nhanvienbanve'
 go
+
+-- nhan vien quan ly
 create login L_nhanvienquanly with password = 'nhanvienquanly'
 go
 create user U_nhanvienquanly for login L_nhanvienquanly
 go
-create role R_NhanVienquanly
-grant select, update, delete on KhachHang to R_NhanVienquanly
-grant select,update, delete on LoaiVe to R_NhanVienquanly
-grant select,update, delete on Phim to R_NhanVienquanly
-grant select,update, delete on SuatChieu to R_NhanVienquanly 
-grant select, update, delete on TheLoai to R_NhanVienquanly
-grant select,update, delete on Ve to R_NhanVienquanly
-grant select,update, delete on VeDangDat to R_NhanVienquanly 
+create role R_NhanVienQuanLy
+grant select, insert, update on NhanVien to R_NhanVienQuanLy
+grant select, update on KhachHang to R_NhanVienQuanLy
+grant select, insert, update on LoaiVe to R_NhanVienQuanLy
+grant select, insert, update on Phim to R_NhanVienQuanLy
+grant select, insert, update on TheLoai to R_NhanVienQuanLy
+grant select, insert, update on SuatChieu to R_NhanVienQuanLy
+grant select, insert, update on PhongChieu to R_NhanVienQuanLy
+grant select, insert, update on KhuyenMai to R_NhanVienQuanLy
+grant select on Ve to R_NhanVienQuanLy
 go
-Sp_AddRoleMember 'R_NhanVienquanly', 'U_nhanvienquanly'
+Sp_AddRoleMember 'R_NhanVienQuanLy', 'U_nhanvienquanly'
+go
